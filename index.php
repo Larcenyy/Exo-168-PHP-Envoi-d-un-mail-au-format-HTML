996e3ee30a4720b39df1935d9c5121f70981673e
Rémy <remyc59440@hotmail.com>
2023-01-19 09:53:15 +0100

    aaa

--- index.php
+++ index.php
@@ -6,11 +6,9 @@
  * Bonus, pour chaque adresse mail indiquer si le mail a bien été envoyer, ou faire un tableau contenant les succès / erreurs et l'afficher après envoi
  *    ex: ['j.conan@fondationface.org' => true, 'toto@fondationface.org' => false] => à vous de trouver la suite !
  */
-
-$to = [
-    'j.conan@fondationface.org',
-    ...
-];
+$to[] = 'remycomeau0@gmail.com';
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset';
 
 $html = '
     <html lang="fr">
@@ -19,9 +17,23 @@ $html = '
         </head>
         <body>
             <div>
-                <!-- Vos actualités ici -->
+                <h1>Bonjour !</h1>
+                <p>Voici les dernières actualités de notre entreprise :</p>
+                <ul>
+                    <li>Actualité 1</li>
+                    <li>Actualité 2</li>
+                    <li>Actualité 3</li>
+                </ul>
+                <p>Nhésitez pas à nous contacter pour plus dinformations.</p>
             </div>
         </body>
     </html>
 ';
 
+foreach ($to as $email) {
+    if (mail($email, 'Mes actualités', $html, implode("\r\n", $headers))) {
+        echo 'Email envoyé a '.$email.' nickel!<br>';
+    } else {
+        echo 'erreur vers '.$email.'<br>';
+    }
+}
